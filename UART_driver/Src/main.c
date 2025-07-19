30c26b1a070607a48d78d9f0b79208a6b54c6833
juanpedro22 <juanpedro.bc22@gmail.com>
2025-07-18 21:47:55 -0300

    add uart rx to the driver

--- UART_driver/Src/main.c
+++ UART_driver/Src/main.c
@@ -7,9 +7,17 @@ int main(void)
 	led_init();
 	uart_tx_init();
 
-	while(1)
-	{
-		printf("Test uart communication \r\n");
-	}
+	while (1)
+	    {
+	        if (frase_completa)
+	        {
+	            frase_completa = false;
+
+	            // Aqui você pode processar a string recebida
+	            printf("Recebido: %s\r\n", rx_buffer);
+	        }
+
+	        // Outras tarefas...
+	    }
 
 }
